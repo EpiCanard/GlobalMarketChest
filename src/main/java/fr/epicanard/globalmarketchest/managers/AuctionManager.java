1fe1b284a4a913804302f010d082f936b6bd4a89
EpiCanard <epicanard@gmail.com>
2018-05-14 12:59:38 +0200

    Clean code

--- src/main/java/fr/epicanard/globalmarketchest/managers/AuctionManager.java
+++ src/main/java/fr/epicanard/globalmarketchest/managers/AuctionManager.java
@@ -4,7 +4,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Timestamp;
-import java.util.Calendar;
 
 import org.bukkit.entity.Player;
 
