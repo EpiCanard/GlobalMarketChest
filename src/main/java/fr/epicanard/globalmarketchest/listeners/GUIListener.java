b31eec254155df63b89051fde21d422940044c99
EpiCanard <epicanard@gmail.com>
2019-01-02 21:43:36 +0100

    Clean from category GUIListener

--- src/main/java/fr/epicanard/globalmarketchest/listeners/GUIListener.java
+++ src/main/java/fr/epicanard/globalmarketchest/listeners/GUIListener.java
@@ -1,10 +1,5 @@
 package fr.epicanard.globalmarketchest.listeners;
 
-import java.lang.reflect.Field;
-
-import org.apache.commons.lang3.reflect.FieldUtils;
-import org.bukkit.Material;
-import org.bukkit.craftbukkit.v1_12_R1.util.CraftMagicNumbers;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,13 +11,12 @@ import org.bukkit.event.inventory.InventoryDragEvent;
 import fr.epicanard.globalmarketchest.GlobalMarketChest;
 import fr.epicanard.globalmarketchest.gui.InventoryGUI;
 import fr.epicanard.globalmarketchest.gui.shops.ShopInterface;
-import net.minecraft.server.v1_12_R1.CreativeModeTab;
-import net.minecraft.server.v1_12_R1.Item;
 
 /**
  * Listener for each interaction done inside a chest
  */
 public class GUIListener implements Listener {
+
   @EventHandler
   public void onClick(InventoryClickEvent event) {
     if (!(event.getWhoClicked() instanceof Player))
@@ -37,7 +31,7 @@ public class GUIListener implements Listener {
       event.setCancelled(true);
 
     ShopInterface interf = inv.getInterface();
-      
+
     if (inv.inventoryEquals(event.getClickedInventory())) {
       event.setCancelled(true);
       if (event.getAction() == InventoryAction.PLACE_ALL || event.getAction() == InventoryAction.PLACE_ONE || event.getAction() == InventoryAction.SWAP_WITH_CURSOR)
@@ -53,7 +47,7 @@ public class GUIListener implements Listener {
     }
 
   }
-  
+
   @EventHandler
   public void onDrag(InventoryDragEvent event) {
     if (event.getWhoClicked() instanceof Player) {
@@ -64,25 +58,4 @@ public class GUIListener implements Listener {
     }
   }
 
-  // TO DELETE : Not Working for every item (See if it's work better in 1.13)
-  public void getCategoryByMystery(Material item) {
-    Item it = CraftMagicNumbers.getItem(item);
-    try {
-      System.out.println(it.getName());
-      Field[] lst = FieldUtils.getAllFields(it.getClass());
-      for (Field a : lst) {
-        if (a.getType() == CreativeModeTab.class) {
-          CreativeModeTab ret = (CreativeModeTab) FieldUtils.readField(a, it, true);
-          if (ret != null) {
-            System.out.println("==> " + FieldUtils.readField(ret, "p", true));
-          }
-        }
-      }
-    } catch (IllegalArgumentException e) {
-      System.out.println("marche pas");
-    } catch (IllegalAccessException e) {
-      System.out.println("marche pas illegal");
-    }
-
-  }
 }
