75f713547aeb9bdf8f5ab39017d2ebe122b3b1ff
EpiCanard <epicanard@gmail.com>
2019-03-10 20:01:38 +0100

    Add auction view base for similar auction views

--- src/main/java/fr/epicanard/globalmarketchest/gui/shops/interfaces/AuctionViewList.java
+++ src/main/java/fr/epicanard/globalmarketchest/gui/shops/interfaces/AuctionViewList.java
@@ -1,10 +1,7 @@
 package fr.epicanard.globalmarketchest.gui.shops.interfaces;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
 
-import org.apache.commons.lang3.tuple.Pair;
 import org.bukkit.inventory.ItemStack;
 
 import fr.epicanard.globalmarketchest.GlobalMarketChest;
@@ -12,18 +9,14 @@ import fr.epicanard.globalmarketchest.auctions.AuctionInfo;
 import fr.epicanard.globalmarketchest.auctions.AuctionLoreConfig;
 import fr.epicanard.globalmarketchest.gui.InventoryGUI;
 import fr.epicanard.globalmarketchest.gui.TransactionKey;
-import fr.epicanard.globalmarketchest.gui.actions.PreviousInterface;
-import fr.epicanard.globalmarketchest.gui.shops.baseinterfaces.DefaultFooter;
+import fr.epicanard.globalmarketchest.gui.shops.baseinterfaces.AuctionViewBase;
 import fr.epicanard.globalmarketchest.managers.GroupLevels;
-import fr.epicanard.globalmarketchest.permissions.Permissions;
 import fr.epicanard.globalmarketchest.shops.ShopInfo;
 import fr.epicanard.globalmarketchest.utils.DatabaseUtils;
 import fr.epicanard.globalmarketchest.utils.ItemStackUtils;
-import fr.epicanard.globalmarketchest.utils.PlayerUtils;
 import fr.epicanard.globalmarketchest.utils.Utils;
 
-public class AuctionViewList extends DefaultFooter {
-  private List<Pair<ItemStack, AuctionInfo>> auctions = new ArrayList<>();
+public class AuctionViewList extends AuctionViewBase {
   private GroupLevels level;
   private String category;
   private ItemStack item;
@@ -42,7 +35,7 @@ public class AuctionViewList extends DefaultFooter {
       GlobalMarketChest.plugin.auctionManager.getAuctions(this.level, shop.getGroup(), this.category, item, this.paginator.getLimit(),
           auctions -> {
             if (pag.getLimit().getLeft() == 0 || auctions.size() > 0)
-              this.auctions = auctions;
+              this.auctions = Utils.mapList(auctions, auction -> auction.getRight());
             pag.setItemStacks(Utils.mapList(auctions, auction -> {
               ItemStack it = auction.getLeft();
               int max = inv.getInv().getMaxStackSize();
@@ -56,8 +49,6 @@ public class AuctionViewList extends DefaultFooter {
     });
 
     this.paginator.setClickConsumer(this::selectAuction);
-
-    this.actions.put(0, new PreviousInterface());
   }
 
   @Override
@@ -66,21 +57,22 @@ public class AuctionViewList extends DefaultFooter {
     super.load();
   }
 
-  private void selectAuction(Integer pos) {
+  /**
+   * This method is called when player click on item inside paginator
+   * If it is the last level it called the super selectAuction method
+   * Else it load a new interface to the next level
+   * 
+   * @param pos Position clicked inside paginator
+   */
+  @Override
+  protected void selectAuction(Integer pos) {
     if (pos >= this.auctions.size() || pos < 0)
       return;
-    AuctionInfo auction = this.auctions.get(pos).getRight();
-    Boolean isOwner = auction.getPlayerStarter().equals(PlayerUtils.getUUIDToString(this.inv.getPlayer()));
+      
+    AuctionInfo auction = this.auctions.get(pos);
 
     if (this.level.getNextLevel(category) == null) {
-      if (!isOwner && !Permissions.GS_BUYAUCTION.isSetOnWithMessage(this.inv.getPlayer()))
-        return;
-
-      this.inv.getTransaction().put(TransactionKey.AUCTIONINFO, auction);
-      if (isOwner)
-        this.inv.loadInterface("EditAuction");
-      else
-        this.inv.loadInterface("BuyAuction");
+      super.selectAuction(pos);
     } else {
       this.inv.getTransaction().put(TransactionKey.GROUPLEVEL, this.level.getNextLevel(category));
       this.inv.getTransaction().put(TransactionKey.AUCTIONITEM, DatabaseUtils.deserialize(auction.getItemMeta()));
