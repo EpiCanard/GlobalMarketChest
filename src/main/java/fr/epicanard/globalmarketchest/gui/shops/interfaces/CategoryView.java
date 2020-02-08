62d211e2386c24c7f42016b1769d94b448d549d4
EpiCanard <epicanard@gmail.com>
2020-02-09 00:09:44 +0100

    Set default lastAuction option value to false to prevent error

--- src/main/java/fr/epicanard/globalmarketchest/gui/shops/interfaces/CategoryView.java
+++ src/main/java/fr/epicanard/globalmarketchest/gui/shops/interfaces/CategoryView.java
@@ -11,11 +11,11 @@ import fr.epicanard.globalmarketchest.gui.shops.baseinterfaces.DefaultFooter;
 import fr.epicanard.globalmarketchest.managers.GroupLevels;
 
 public class CategoryView extends DefaultFooter {
-  private Boolean lastAuctionsEnabled = false;
+  private Boolean lastAuctionsEnabled;
 
   public CategoryView(InventoryGUI inv) {
     super(inv);
-    this.lastAuctionsEnabled = GlobalMarketChest.plugin.getConfigLoader().getConfig().getBoolean("Options.EnableLastAuctions", true);
+    this.lastAuctionsEnabled = GlobalMarketChest.plugin.getConfigLoader().getConfig().getBoolean("Options.EnableLastAuctions", false);
     this.actions.put(0, new NextInterface("SearchView"));
     if (this.lastAuctionsEnabled) {
       this.actions.put(1, new NextInterface("LastAuctionViewList"));
