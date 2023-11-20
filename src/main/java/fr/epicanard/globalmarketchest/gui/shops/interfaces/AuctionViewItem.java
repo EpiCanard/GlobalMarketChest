4ab1a82cd39a8d93083522fcf29c7a81d9b48df0
EpiCanard <epicanard@gmail.com>
2023-11-20 20:24:04 +0100

    Add tax in lore and add configs/langs missing

--- src/main/java/fr/epicanard/globalmarketchest/gui/shops/interfaces/AuctionViewItem.java
+++ src/main/java/fr/epicanard/globalmarketchest/gui/shops/interfaces/AuctionViewItem.java
@@ -26,7 +26,7 @@ public class AuctionViewItem extends AuctionViewBase {
             if (pag.getLimit().getLeft() == 0 || auctions.size() > 0)
               this.auctions = auctions;
             pag.setItemStacks(DatabaseUtils.toItemStacks(auctions, (itemstack, auction) -> {
-              ItemStackUtils.addItemStackLore(itemstack, auction.getLore(AuctionLoreConfig.TOSELL));
+              ItemStackUtils.addItemStackLore(itemstack, auction.getLore(AuctionLoreConfig.TOSELL, this.inv.getPlayer()));
             }));
           });
     });
