fd104b839fdf6b07b54dd421ac0d89e358af9635
EpiCanard <epicanard@gmail.com>
2024-07-28 16:32:51 +0200

    Implement all nms version from 1.12 to 1.21

---
+++ modules/v1_15_R1/src/main/java/fr/epicanard/globalmarketchest/nms/Nms_v1_15_R1.java
@@ -0,0 +1,65 @@
+package fr.epicanard.globalmarketchest.nms;
+
+import net.minecraft.server.v1_15_R1.ChatMessage;
+import net.minecraft.server.v1_15_R1.Containers;
+import net.minecraft.server.v1_15_R1.EntityPlayer;
+import net.minecraft.server.v1_15_R1.IRegistry;
+import net.minecraft.server.v1_15_R1.Item;
+import net.minecraft.server.v1_15_R1.MinecraftKey;
+import net.minecraft.server.v1_15_R1.NBTTagCompound;
+import net.minecraft.server.v1_15_R1.PacketPlayOutOpenWindow;
+import org.bukkit.craftbukkit.v1_15_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_15_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class Nms_v1_15_R1 implements Nms {
+
+  @Override
+  public String getMinecraftKey(ItemStack item) {
+    return IRegistry.ITEM.getKey(CraftItemStack.asNMSCopy(item).getItem()).toString();
+  }
+
+  @Override
+  public void updateInventoryName(Player player, String name) {
+    EntityPlayer entityPlayer = ((CraftPlayer)player).getHandle();
+    Integer windowId = entityPlayer.activeContainer.windowId;
+    PacketPlayOutOpenWindow packet = new PacketPlayOutOpenWindow(windowId, Containers.GENERIC_9X6, new ChatMessage(name));
+
+    entityPlayer.playerConnection.sendPacket(packet);
+    entityPlayer.updateInventory(entityPlayer.activeContainer);
+  }
+
+  @Override
+  public ItemStack setNbtTag(ItemStack item) {
+    net.minecraft.server.v1_15_R1.ItemStack nmsItemStack = CraftItemStack.asNMSCopy(item);
+    NBTTagCompound tag = nmsItemStack.getTag();
+
+    if (tag != null && tag.hasKey(GMC_ITEM_TAG))
+      return item;
+
+    if (tag == null)
+      tag = new NBTTagCompound();
+    tag.setBoolean(GMC_ITEM_TAG, true);
+    nmsItemStack.setTag(tag);
+
+    return CraftItemStack.asBukkitCopy(nmsItemStack);
+  }
+
+  @Override
+  public Boolean hasNbtTag(ItemStack item) {
+    NBTTagCompound tag = CraftItemStack.asNMSCopy(item).getTag();
+    return tag != null && tag.hasKey(GMC_ITEM_TAG);
+  }
+
+  @Override
+  public ItemStack getItemStack(String minecraftKey) {
+    Item item = IRegistry.ITEM.get(new MinecraftKey(minecraftKey));
+    return setNbtTag(CraftItemStack.asNewCraftStack(item));
+  }
+
+  @Override
+  public String getItemStackDisplayName(ItemStack item) {
+    return CraftItemStack.asNMSCopy(item).getName().toString();
+  }
+}
---
+++ modules/v1_17_R1/src/main/java/fr/epicanard/globalmarketchest/nms/Nms_v1_17_R1.java
@@ -0,0 +1,64 @@
+package fr.epicanard.globalmarketchest.nms;
+
+import net.minecraft.core.Registry;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.TranslatableComponent;
+import net.minecraft.network.protocol.game.ClientboundOpenScreenPacket;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.inventory.MenuType;
+import net.minecraft.world.item.Item;
+import org.bukkit.craftbukkit.v1_17_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_17_R1.inventory.CraftItemStack;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
+
+public class Nms_v1_17_R1 implements Nms {
+  @Override
+  public String getMinecraftKey(ItemStack item) {
+    return Registry.ITEM.getKey(CraftItemStack.asNMSCopy(item).getItem()).getPath();
+  }
+
+  @Override
+  public void updateInventoryName(Player player, String name) {
+    ServerPlayer serverPlayer = ((CraftPlayer)player).getHandle();
+    Integer containerId = serverPlayer.containerMenu.containerId;
+    TranslatableComponent title = new TranslatableComponent(name);
+    ClientboundOpenScreenPacket packet = new ClientboundOpenScreenPacket(containerId, MenuType.GENERIC_9x6, title);
+
+    serverPlayer.connection.send(packet);
+  }
+
+  @Override
+  public ItemStack setNbtTag(ItemStack item) {
+    net.minecraft.world.item.ItemStack itemStack = CraftItemStack.asNMSCopy(item);
+    CompoundTag tag = itemStack.getTag();
+
+    if (tag != null && tag.contains(GMC_ITEM_TAG))
+      return item;
+
+    if (tag == null)
+      tag = new CompoundTag();
+    tag.putBoolean(GMC_ITEM_TAG, true);
+    itemStack.setTag(tag);
+    return CraftItemStack.asBukkitCopy(itemStack);
+  }
+
+  @Override
+  public Boolean hasNbtTag(ItemStack item) {
+    CompoundTag tag = CraftItemStack.asNMSCopy(item).getTag();
+    return tag != null && tag.contains(GMC_ITEM_TAG);
+  }
+
+  @Override
+  public ItemStack getItemStack(String minecraftKey) {
+    ResourceLocation resourceLocation = new ResourceLocation(minecraftKey);
+    Item item = Registry.ITEM.get(resourceLocation);
+    return setNbtTag(CraftItemStack.asNewCraftStack(item));
+  }
+
+  @Override
+  public String getItemStackDisplayName(ItemStack item) {
+    return CraftItemStack.asNMSCopy(item).getDisplayName().getString();
+  }
+}
